a57b159e240852a899c86ba6cb4bec60f4c6dbd1
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-11 21:42:09 +0300

    kj

--- api/oauth/oauth.go
+++ api/oauth/oauth.go
@@ -8,6 +8,9 @@ import (
 	"github.com/portainer/portainer"
 )
 
+// Service represents a service used to authenticate users against a OAuth.
+type Service struct{}
+
 // AuthenticateUser is used to authenticate a user against a OAuth.
 func (*Service) AuthenticateUser(username string, settings *portainer.OAuthSettings) error {
 
