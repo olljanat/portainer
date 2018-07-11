d19696bd14cfb9f5568234832c6bb12f53cf905e
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-11 21:47:11 +0300

    klk

--- api/oauth/oauth.go
+++ api/oauth/oauth.go
@@ -14,6 +14,6 @@ type Service struct{}
 // AuthenticateUser is used to authenticate a user against a OAuth.
 func (*Service) AuthenticateUser(username string, settings *portainer.OAuthSettings) error {
 
-	err = errors.New("oauth.go - OAuth authentication is not yet implemented")
+	err := errors.New("oauth.go - OAuth authentication is not yet implemented")
 	return err
 }
